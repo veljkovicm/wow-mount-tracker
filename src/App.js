3e21e73e1b68df0e87b66e1c89663a5d18007358
Ćomi <veljkovicwebdev@gmail.com>
2020-09-18 16:51:07 +0200

    Adds default route

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/mounts/:region/:realm/:character" render={(props) => <Mounts {...props}/>} />
+          {/* TODO: Add 404 component */}
+          <Route component={Home} />
         </Switch>
       </Router>
     </div>
