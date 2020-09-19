eb113432a55543945da1653e420e6466073b238b
Ćomi <veljkovicwebdev@gmail.com>
2020-09-19 09:53:00 +0200

    WIP: Adds search functionality

--- src/components/MountList/MountList.jsx
+++ src/components/MountList/MountList.jsx
@@ -5,9 +5,10 @@ const MountList = (props) => {
 
   const [ filter, setFilter ] = useState();
   const [ active, setActive ] = useState('all');
+  const [ search, setSearch ] = useState('');
   
   let count = userMounts.length;
-  
+
   let filteredMounts = mounts;
   mounts.map((mount, i) => {
     if(userMounts.includes(mount.id)) {
@@ -23,19 +24,30 @@ const MountList = (props) => {
     count = filteredMounts.length;
   }
 
-
+  if(search) {
+    filteredMounts = filteredMounts.filter(
+      mount => mount.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }
 
   let markup = (
     filteredMounts.map((mount, i) => {
       return <div className={`mount-item ${mount.collected ? 'collected': ''}`} key={i}>
         <div
           className="mount-item-image"
-          style={{backgroundImage:`url(https://render-us.worldofwarcraft.com/npcs/zoom/creature-display-${mount.creature_displays[0].id}.jpg)`}}>
-          </div>
-          <p>{mount.name}</p>
+          style={{
+            backgroundImage:`url(https://render-us.worldofwarcraft.com/npcs/zoom/creature-display-${mount.creature_displays[0].id}.jpg)`
+          }}
+        >
+        </div>
+        { mount.faction ? <img src={`/${mount.faction.name}.png`} alt="faction-symbol" /> : null }
+        <p>{mount.name}</p>
       </div>
     })
   )
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  }
 
   const handleButton = (event) => {
     setActive(event.target.name);
@@ -48,11 +60,16 @@ const MountList = (props) => {
 
   return (
     <div className="mounts">
-      <div className="controls-wrapper">
-        <p>Count: {count}</p>
-        <button onClick={handleButton} name="all" className={active === 'all' ? 'active' : ''}>all</button>
-        <button onClick={handleButton} name="uncollected" className={active === 'uncollected' ? 'active' : ''}>uncollected</button>
-        <button onClick={handleButton} name="collected" className={active === 'collected' ? 'active' : ''}>collected</button>
+      <div>
+        <div className="controls-wrapper">
+          <p>Count: {count}</p>
+          <button onClick={handleButton} name="all" className={active === 'all' ? 'active' : ''}>All</button>
+          <button onClick={handleButton} name="collected" className={active === 'collected' ? 'active' : ''}>Collected</button>
+          <button onClick={handleButton} name="uncollected" className={active === 'uncollected' ? 'active' : ''}>Uncollected</button>
+        </div>
+        <div>
+          <input type="text" name="search" value={search} onChange={handleSearch}/>
+        </div>
       </div>
       <div className="mount-list__wrapper">
         {markup}
