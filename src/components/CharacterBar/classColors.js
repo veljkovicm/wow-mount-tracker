33c9ecf9eebf4f046ac1d1f30cd68d4b311592b7
Ćomi <veljkovicwebdev@gmail.com>
2020-09-20 18:18:18 +0200

    Adds class colors css

---
+++ src/components/CharacterBar/classColors.js
@@ -0,0 +1,14 @@
+export default {
+  Druid: '#ff7c0a',
+  Shaman: '#2359ff',
+  Hunter: '#aad372',
+  Monk: '#00ffba',
+  Mage: '#68ccef',
+  Warrior: '#c69b6d',
+  Paladin: '#f48cba',
+  DemonHunter: '#a330c9',
+  Rogue: '#fff468', 
+  DeathKnight: '#c41e3b',
+  Priest: '#f0ebe0',
+  Warlock: '#9382c9',
+}
