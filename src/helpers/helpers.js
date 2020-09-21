b4a3e06c3762739bd1079c929cfdb98c10945374
Milan <comiv92@gmail.com>
2020-09-21 17:10:55 +0200

    Adds helper functions

---
+++ src/helpers/helpers.js
@@ -0,0 +1,12 @@
+export const mergeArrayObjects = (arr1, arr2) => {
+  return arr1.map((item,i)=>{
+    if(item.id === arr2[i].id){
+      //merging two objects
+      return Object.assign({},item,arr2[i])
+    }
+  })
+}
+
+export const sortData = (data, field = 'name') => {
+  return data.sort((a,b) => (a[field] > b[field]) ? 1 : ((b[field] > a[field]) ? -1 : 0));
+}
