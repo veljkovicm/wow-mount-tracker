656b1f5f9405796726aa333985bc21edcd4e301b
Milan <comiv92@gmail.com>
2021-02-17 10:46:53 +0100

    Fix: add predefined entry button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Form from '../../components/Form/Form';
 
 import './home.css';
 
 const Home = () => {
+  const history = useHistory();
+
   return (
     <div className="home-container">
       <Form />
+      <div className="home-link-container">
+        <p>or</p>
+        <button onClick={() => history.push('/mounts/eu/kazzak/tebra')}>Proceed with a predefined entry</button>
+      </div>
     </div>
   )
 }
