6b2a5497c0c1b71f7deab08d70bb76ce8802a004
Milan <comiv92@gmail.com>
2021-02-22 23:02:54 +0100

    Fix: fix class property

--- src/pages/Mounts/components/Loading/Loading.jsx
+++ src/pages/Mounts/components/Loading/Loading.jsx
@@ -5,7 +5,7 @@ import './loading.css';
 export default function Loading() {
   return (
     <div className="spinner-wrapper">
-      <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+      <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     </div>
   )
 }
